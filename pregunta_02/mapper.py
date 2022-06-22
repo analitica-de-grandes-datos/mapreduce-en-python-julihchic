d790a98b8f42be16d43c1066ae081e92c57839ff
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-22 17:25:29 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+for row in sys.stdin:
+    campo = row.split(',')
+    prop = campo[3]
+    mont = campo[4]
+
+    sys.stdout.write(f'{mont}\t{prop}\n')
