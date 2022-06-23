7a896d4149affc8ff5e9d827fdf4a7f1fb9bd275
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 10:25:24 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    for row in sys.stdin:
+        data= row.split('\t')
+
+        sys.stdout.write(f'{row}\n')
