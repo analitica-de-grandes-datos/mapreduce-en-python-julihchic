0d063a47d82c62675acb41dbbf112ca433446e3f
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 15:52:14 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    for row in sys.stdin:
+        campo = row.strip().split('   ')
+        ltr = campo[0]
+        fecha = campo[1]
+        num = campo[2]
+
+        sys.stdout.write(f'{ltr}\t{fecha}\t{num}\n')
