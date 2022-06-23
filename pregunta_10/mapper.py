7f72227e77446fea77b097ece3c5735adc1ee866
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 16:08:32 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    for row in sys.stdin:
+        campo = row.strip().split('\t')
+        clave = campo[0]
+        letr = campo[1]
+        
+
+        sys.stdout.write(f'{clave}\t{letr}\n')
