82667ee868565b6a6024630a1895b00ec1e8b75d
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 11:38:39 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,37 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    Keyname = None
+    max = 0
+    min = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split('\t')
+        val = float(val)
+
+
+        if key == Keyname:
+            if val > max:
+                max = val
+            else:
+                val = max
+            
+            if val < min:
+                min = val
+            else:
+                val = min 
+                                 
+        else:
+            if Keyname is not None:
+                sys.stdout.write('{}\t{}\t{}\n'.format(Keyname, max, min))
+
+                Keyname = key
+                max = val
+                min = val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(Keyname, max, min))
