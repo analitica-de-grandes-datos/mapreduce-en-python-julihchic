c8ff42d87cdd350f55f5bd44bcdc3a8d2118eec0
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 15:46:01 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -6,18 +6,16 @@ import sys
 
 if __name__ == '__main__':
 
-    list = []
+    lista = []
+
+    for line in sys.stdin: 
+        key, fecha, valor = line.split("\t")
+        valor = int(valor)
+        tupla = key, fecha, valor
+        lista.append(tupla)
+          
+    sortedDict = sorted(lista, key=operator.itemgetter(0, 2))
     
-
-    for line in sys.stdin:
-
-        key, fecha, val = line.split('\t')
-        val = int(val)
-        mi_t = key, fecha, val
-        list.append(mi_t)
-
-    sortedDict = sorted(list, key = operator.itemgetter(0, 2))
-    for line in sortedDict:
-              
-
-        sys.stdout.write('{}  {}  {}\n'.format(line[0], line[1], line[2]))
+    for linea in sortedDict:
+        
+        sys.stdout.write("{}   {}   {}\n".format(linea[0], linea[1], linea[2]))
