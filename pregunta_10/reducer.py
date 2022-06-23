1cb510de802dc6301999e680ca7f52c710d411ae
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 16:08:35 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from dataclasses import replace
+import operator
+from collections import defaultdict
+import sys
+
+if __name__ == '__main__':
+
+    Dict = defaultdict(list)
+
+    for line in sys.stdin:
+
+        key, val = line.replace('\n', '').strip().split('\t')
+        val = val.split(',')
+
+        for letra in val:
+            Dict[letra].append(key)
+
+    sortedDict = sorted(Dict.items(), key = operator.itemgetter(0))
+
+    for clv, vlr in sortedDict:
+        result = sorted(vlr, key = int)
+        vlr =','.join(map(str, result))
+
+     
+        sys.stdout.write("{}\t{}\n".format(clv, vlr))
