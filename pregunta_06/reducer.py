b96b7b1477424be3f7fe1bfa6c0b7085c4931fbd
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 11:56:35 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -6,36 +6,39 @@ from curses import keyname
 import sys
 
 
-
 if __name__ == '__main__':
 
     keyname = None
     max = 0
     min = 0
+    
 
     for line in sys.stdin:
-
-        key, val = line.split('\t')
+        
+        key, val = line.split("\t")
         val = float(val)
-
-
+              
         if key == keyname:
+        
             if val > max:
                 max = val
             else:
                 val = max
-            
+
             if val < min:
                 min = val
             else:
-                val = min 
-                                 
-        else:
-            if keyname is not None:
-                sys.stdout.write('{}\t{}\t{}\n'.format(keyname, max, min))
+                val = min
 
-                keyname = key
-                max = val
-                min = val
+         
+        else:
+         
+            if keyname is not None:
+
+                sys.stdout.write("{}\t{}\t{}\n".format(keyname, max, min))
+
+            keyname = key
+            max = val
+            min = val
 
     sys.stdout.write("{}\t{}\t{}\n".format(keyname, max, min))
