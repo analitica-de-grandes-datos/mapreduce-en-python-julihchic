cec3165301c972dddf9736f005d36d6d97310d69
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 15:07:59 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,23 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import operator
+import sys
+
+if __name__ == '__main__':
+
+    list = []
+    
+
+    for line in sys.stdin:
+
+        key, fecha, val = line.split('\t')
+        val = int(val)
+        mi_t = key, fecha, val
+        list.append(mi_t)
+
+    sortedDict = sorted(list, key = operator.itemgetter(0, 2))
+    for line in sortedDict:
+              
+
+        sys.stdout.write('{}\t{}\t{}\n'.format(line[0], line[1], line[2]))
