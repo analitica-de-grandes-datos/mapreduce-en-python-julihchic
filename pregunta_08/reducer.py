1e3655e59ff0382be72893fc069a31c145f57313
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 15:42:57 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -6,21 +6,27 @@ import sys
 import operator
 from traceback import StackSummary
 
+
 if __name__ == '__main__':
+
 
     Dict = defaultdict(list)
 
 
     for line in sys.stdin:
 
+
         key, val = line.split('\t')
         val = float(val)
         Dict[key].append(val)
+
 
     for clave, lista in Dict.items():
         suma = round(sum(lista), 1)
         prom = sum(lista)/len(lista)
 
+
         values= [clave, suma, prom]
+        
 
     sys.stdout.write("{}\t{}\t{}\n".format(values[0], values[1], values[2]))
