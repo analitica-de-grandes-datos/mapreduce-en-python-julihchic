8f4ac5e589b0c1bb05218a209adba4498975fea2
julihchic <101681736+julihchic@users.noreply.github.com>
2022-06-23 15:36:53 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from collections import defaultdict
+import sys
+
+if __name__ == '__main__':
+
+    Dict = defaultdict(list)
+
+
+    for line in sys.stdin:
+
+        key, val = line.split('\t')
+        val = float(val)
+        Dict[key].append(val)
+
+    for clave, lista in Dict.items():
+        suma = round(sum(lista), 1)
+        prom = sum(lista)/len(lista)
+
+        values= [clave, suma, prom]
+
+    sys.stdout.write("{}\t{}\t{}\n".format(values[0], values[1], values[2]))
